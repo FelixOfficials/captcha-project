21f30076754a41894f16f4e39e88f96e55c366b5
FelixDNguyenn <104798436+FelixOfficials@users.noreply.github.com>
2024-01-09 08:46:20 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,55 +1,70 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 import os
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
+root = ctk.CTk()
 root.geometry("348x460")
 root.resizable(width=True, height=True)
 
 def login():
     print("Test")
 
-frame = customtkinter.CTkFrame(master=root)
+frame = ctk.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System")
+label = ctk.CTkLabel(master=frame, text="Login System")
 label.pack(pady=12, padx=10)
 
-username = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+username = ctk.CTkEntry(master=frame, placeholder_text="Username")
 username.pack(pady=12, padx=10)
 
-password = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+password = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
 password.pack(pady=12, padx=10)
 
-email = customtkinter.CTkEntry(master=frame, placeholder_text="Email")
+email = ctk.CTkEntry(master=frame, placeholder_text="Email")
 email.pack(pady=12, padx=10)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Captcha")
-checkbox.pack(pady=12, padx=10)
+check_var = ctk.StringVar(value="0")
 
-button = customtkinter.CTkButton(master=frame, text="Login", command=login)
-button.pack(pady=12, padx=10)
+def captcha_dropdown():
+    if check_var.get() == "1":
+        my_label.configure(text="you clicked")
+    else:
+        my_label.configure(text="")
 
-def button_state():
-    if customtkinter.CHECKBUTTON() == 1:
-        open_captcha()
-
-def open_captcha():
-    captcha = customtkinter.CTkToplevel(master=frame)
+    captcha = ctk.CTkToplevel(master=frame)
     captcha.title("Captcha Frame")
 
-    label2 = customtkinter.CTkLabel(master=frame, text="Login System")
+    label2 = ctk.CTkLabel(master=frame, text="Login System")
     label2.pack(pady=12, padx=10)
+
+checkbox = ctk.CTkCheckBox(master=frame, 
+                           text="I'm not a robot (Fake Captcha)", 
+                           variable=check_var, 
+                           onvalue="1", 
+                           offvalue="0",
+                           command=captcha_dropdown)
+checkbox.pack(pady=12, padx=10)
+
+my_label = ctk.CTkLabel(master=frame, text="")
+my_label.pack(pady=20)
+
+button = ctk.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+combobox = ctk.CTkOptionMenu(
+    master=frame,
+    values=["Captcha 1"],
+)
+combobox.pack(pady=12, padx=10)
+
 
 def main():
     root.mainloop()
 
 if __name__ == "__main__":
     main()
-
-
-#https://www.youtube.com/watch?v=mop6g-c5HEY (Video to learn from)
