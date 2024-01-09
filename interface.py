140d9374720040ce8a9efb075df7512540d75ac3
FelixDNguyenn <104798436+FelixOfficials@users.noreply.github.com>
2024-01-09 08:17:01 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,10 +1,14 @@
+from PIL import Image, ImageDraw, ImageFont
+import random
+import os
 import customtkinter
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("500x350")
+root.geometry("348x460")
+root.resizable(width=True, height=True)
 
 def login():
     print("Test")
@@ -15,16 +19,37 @@ frame.pack(pady=20, padx=60, fill="both", expand=True)
 label = customtkinter.CTkLabel(master=frame, text="Login System")
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady=12, padx=10)
+username = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+username.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry1.pack(pady=12, padx=10)
+password = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+password.pack(pady=12, padx=10)
+
+email = customtkinter.CTkEntry(master=frame, placeholder_text="Email")
+email.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Captcha")
+checkbox.pack(pady=12, padx=10)
 
 button = customtkinter.CTkButton(master=frame, text="Login", command=login)
 button.pack(pady=12, padx=10)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
-checkbox.pack(pady=12, padx=10)
+def button_state():
+    if customtkinter.CHECKBUTTON() == 1:
+        open_captcha()
 
-root.mainloop()
+def open_captcha():
+    captcha = customtkinter.CTkToplevel(master=frame)
+    captcha.title("Captcha Frame")
+
+    label2 = customtkinter.CTkLabel(master=frame, text="Login System")
+    label2.pack(pady=12, padx=10)
+
+def main():
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+#https://www.youtube.com/watch?v=mop6g-c5HEY (Video to learn from)
